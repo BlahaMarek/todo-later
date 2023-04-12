3c2f7caaa682e03f4914f6ba5392c88fd444c92d
Marek Blaha <blaha.marcus@gmail.com>
2023-04-12 18:23:37 +0200

    todo later

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,154 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
+    <ion-header>
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Todo later</ion-title>
+        <ion-buttons slot="end">
+          <ion-icon class="ion-margin-end" :icon="settingsOutline" @click="openBrowser"  @touchstart="mouseDown" @touchend="mouseUp"></ion-icon>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
 
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    <ion-content :fullscreen="true" class="ion-padding">
+
+      <div>
+        <ion-textarea
+            v-model="textArea"
+            auto-grow
+            placeholder="Type something here"></ion-textarea>
       </div>
+
     </ion-content>
+    <ion-footer>
+      <div class="footer-button" @click="saveText" >Save</div>
+      <br><br>
+    </ion-footer>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script lang="ts">
+import { IonContent, IonHeader, IonPage, IonToolbar, IonTextarea, IonFooter, IonIcon, IonButtons, IonTitle } from '@ionic/vue'
+import { Browser } from '@capacitor/browser'
+import { settingsOutline } from 'ionicons/icons'
+import { CapacitorVolumeButtons } from 'capacitor-volume-buttons'
+import axios from 'axios'
+import moment from 'moment'
+
+export default {
+  data() {
+    return {
+      textArea: '',
+      settingsOutline,
+      timestamps: [],
+      timer: null,
+      logs: null
+    }
+  },
+
+  components: {
+    IonContent, IonHeader, IonPage, IonToolbar, IonTextarea, IonFooter, IonIcon, IonButtons, IonTitle
+  },
+
+  created() {
+    this.logs = JSON.parse(localStorage.getItem('logs'))
+    const data =
+      {
+        date: moment().format('DD.MM.YYYY, h:mm:ss.SSSS'),
+        event: 'App opened',
+        status: '-'
+      }
+
+    if (!this.logs) {
+      return localStorage.setItem('logs', JSON.stringify([data]))
+    }
+
+    this._log('App opened')
+  },
+
+  mounted() {
+    this.textArea = localStorage.getItem('textArea')
+    this._log('Text saved')
+    CapacitorVolumeButtons.addListener('volumeButtonPressed', this.onVolumeButtonPressed)
+  },
+
+  methods: {
+    saveText() {
+      localStorage.setItem('textArea', this.textArea)
+    },
+
+    mouseDown() {
+      console.log('DOWN')
+      this.timer = setTimeout(() => {
+        this.$router.push('/logs')
+      }, 2000)
+    },
+
+    mouseUp() {
+      console.log('up')
+      clearTimeout(this.timer)
+    },
+
+    async openBrowser() {
+      if (this.timestamps.length < 4) {
+        return this.timestamps.push(new Date())
+      }
+
+      this.timestamps.push(new Date())
+      this.timestamps.shift()
+
+      const difference = this.timestamps[3].getTime() - this.timestamps[0].getTime()
+
+      if (difference < 2000) {
+        this._log('Browser opened')
+        await Browser.open({ url: 'https://dev.iwesys.com/b_todo/' })
+      }
+    },
+
+    async  onVolumeButtonPressed({direction} ) {
+      try {
+        if (direction === 'up') {
+          this._log('UP pressed')
+          const data = await axios.post('https://dev.iwesys.com/b_todo/?type=1')
+          this._log('UP pressed response', data.statusText)
+
+        } else {
+          this._log('DOWN pressed')
+          const data = await axios.post('https://dev.iwesys.com/b_todo/?type=2')
+          this._log('UP pressed response', data.statusText)
+
+        }
+      } catch (e) {
+        console.error(e)
+      }
+    },
+
+    _log(event, status = '-') {
+      this.logs.unshift(
+          {
+            date: moment().format('DD.MM.YYYY, h:mm:ss.SSSS'),
+            event: event,
+            status: '-'
+          })
+      localStorage.setItem('logs', JSON.stringify(this.logs))
+    }
+  }
+}
+
 </script>
 
 <style scoped>
-#container {
+ion-footer {
+  background: #3880ff;
   text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
 }
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
+.footer-button {
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
 }
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+ion-icon {
+  font-size: 25px;
 }
 </style>
---
+++ src/views/LogsPage.vue
@@ -0,0 +1,93 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Logs</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+
+    <ion-content :fullscreen="true" class="ion-padding">
+      <table v-if="logs && logs.length">
+        <thead>
+          <tr>
+            <td>Date</td>
+            <td>Event</td>
+            <td>Status</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="log in logs" :key="log">
+            <td>{{log.date}}</td>
+            <td>{{log.event}}</td>
+            <td>{{log.status}}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div v-else class="ion-text-center ion-padding">
+        No logs in storage
+      </div>
+
+      <ion-button v-if="logs && logs.length" color="danger" fill="outline" expand="block" @click="clearStorage" >Remove logs</ion-button>
+
+    </ion-content>
+    <ion-footer>
+      <div class="footer-button" @click="goBack" >Return back</div>
+      <br><br>
+    </ion-footer>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonContent, IonHeader, IonPage, IonToolbar, IonButton, IonFooter, IonTitle } from '@ionic/vue'
+import { settingsOutline } from 'ionicons/icons'
+
+export default {
+  data() {
+    return {
+      logs: []
+    }
+  },
+
+  components: {
+    IonContent, IonHeader, IonPage, IonToolbar, IonFooter, IonTitle, IonButton
+  },
+
+  created() {
+    this.logs = JSON.parse(localStorage.getItem('logs'))
+  },
+
+
+  methods: {
+    goBack() {
+      this.$router.push('/home')
+    },
+
+    clearStorage() {
+      this.logs = []
+      localStorage.removeItem('logs')
+    }
+
+  }
+}
+
+</script>
+
+<style scoped>
+ion-footer {
+  background: #3880ff;
+  text-align: center;
+}
+.footer-button {
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+}
+tbody {
+  font-size: 0.8em;
+}
+td {
+  padding: 3px 6px;
+}
+</style>
